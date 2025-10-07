9b9ab3fce6e6cba46ec5e76785f47a9505dbdbb8
T・K <karuma.reason@gmail.com>
2025-10-07 13:05:08 +0900

    Default.

---
+++ req/rubygems.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# dependent file or library.
+# --------------------------------------
+require 'version'
+require 'bmi'
+# --------------------------------------
+
+# File encoding setting is UTF-8.
+# --------------------------------------
+encoding_style
+# --------------------------------------
