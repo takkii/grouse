4287b3a1ec2b09349db27771a43afff0755a1e69
T・K <karuma.reason@gmail.com>
2025-10-07 13:05:04 +0900

    Default.

---
+++ lib/version.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Grs
+  def self.version
+    '1.0.0'.to_s
+  end
+end
+
+begin
+    Grs.version
+rescue StandardError => e
+    puts e.backtrace
+ensure
+    GC.auto_compact
+end
+
+__END__
